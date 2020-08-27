ec932fd0c1b3a6f58066de5b7f43fe4c5ea26e96
Jack <yichen.yao1@gmail.com>
2020-08-26 22:08:12 -0400

    added function to check if sudoku position is valid

--- sudoku.py
+++ sudoku.py
@@ -12,6 +12,7 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+# prints sudoku board
 def print_board(bo):
 
     # prints horizontal and vertical dividers every 3 rows and columns
@@ -37,4 +38,26 @@ def find_empty(bo):
             if bo[i][j] == 0:
                 return(i, j )
 
-print_board(board)
+
+# checks if the current board is valid for sudoku
+def valid(bo, num, pos):
+
+    # checks each row and see if # (tuple) is equal to num we just added in
+    # ignores if position is the position we just inserted into
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1]!= i:
+            return False
+
+    # checks which box we are in
+    # integer div to ensure its a whole number
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3)
+        for j in range(box_x * 3, box_x * 3 + 3)
+             if bo[i][j] == num and (i, j) != pos:
+                 return False
+
+
+# print_board(board)
+
