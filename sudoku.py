7f662181d3ad4114eaa8cdfdc6c81dec2f960334
Yichen Yao <yichen.yao1@gmail.com>
2022-06-20 23:59:59 -0400

    modified findEmpty

--- sudoku.py
+++ sudoku.py
@@ -35,10 +35,29 @@ def printBoard(board):
 
 # finds empty square denoted by 0 
 def findEmpty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
+
+    for i in range(len(board)): # scans row
+        for j in range(len(board[0])): # scans column, board[0] = length of 1st list
             if board[i][j] == 0:
                 return (i, j) # row, col
+
+
+
+# as each number is entered into the empty position, checks if the current board
+#  (row/column) is valid for sudoku.
+#
+# "number" is the value inserted
+def valid(board, number, position):
+
+    # scan and checks row; don't check current position
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # scan and checks column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
 
 printBoard(board)
 # print(len(board[0]))
